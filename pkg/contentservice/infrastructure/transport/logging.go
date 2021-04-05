31474add8779168deb08758939f279a7623fc16a
UsingCoding <extendedmoment@outlook.com>
2021-04-05 20:50:00 +0300

    Добавил приём activityID со стороны сервера

--- pkg/contentservice/infrastructure/transport/logging.go
+++ pkg/contentservice/infrastructure/transport/logging.go
@@ -1,13 +1,20 @@
 package transport
 
 import (
+	"contentservice/pkg/common/infrastructure/logging/activity"
 	"context"
 	"fmt"
+	"github.com/pkg/errors"
 	logger "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"net/http"
 	"strings"
 	"time"
+)
+
+var (
+	ErrFailedToReadFromIncomingContext = errors.New("failed to read metadata from context")
 )
 
 func NewLoggingMiddleware(h http.Handler) http.Handler {
@@ -16,26 +23,42 @@ func NewLoggingMiddleware(h http.Handler) http.Handler {
 		h.ServeHTTP(writer, request)
 
 		logger.WithFields(logger.Fields{
-			"duration":  time.Since(now),
-			"method":    request.Method,
-			"url":       request.URL,
-			"userAgent": request.UserAgent(),
+			"duration": time.Since(now),
+			"method":   request.Method,
+			"url":      request.RequestURI,
 		}).Info("request finished")
 	})
 }
 
 func NewLoggerServerInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			logger.WithFields(logger.Fields{
+				"args":   req,
+				"method": getGRPCMethodName(info),
+			}).Error("failed to read metadata from request. Aborting request")
+			return nil, ErrFailedToReadFromIncomingContext
+		}
+
+		activityID, err := activity.ParseActivityID(md.Get("activityID")[0])
+		if err != nil {
+			logger.WithFields(logger.Fields{
+				"args":   req,
+				"method": getGRPCMethodName(info),
+			}).Error("failed to parse activity id from request. Aborting request")
+			return nil, ErrFailedToReadFromIncomingContext
+		}
+
 		start := time.Now()
 
 		resp, err = handler(ctx, req)
 
-		duration := fmt.Sprintf("%v", time.Since(start))
-
 		fields := logger.Fields{
-			"args":     req,
-			"duration": duration,
-			"method":   getGRPCMethodName(info),
+			"activityID": activityID.String(),
+			"args":       req,
+			"duration":   fmt.Sprintf("%v", time.Since(start)),
+			"method":     getGRPCMethodName(info),
 		}
 
 		entry := logger.WithFields(fields)
